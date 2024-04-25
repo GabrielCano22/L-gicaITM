7c304e0c6011aa91788ee0f3146f6b079cb9fceb
Gabriel Jaime Cano Oquendo <gabrieljaimecanooquendo@gmail.com>
2024-04-25 17:32:13 -0500

    25/04/2024

--- JOptionpaneEjemplo.java
+++ JOptionpaneEjemplo.java
@@ -1,19 +1,19 @@
-import javax.swing.JOptionPane;
+// import javax.swing.JOptionPane;
 
-public class jop{
-    public static void main(String[] args) {
-        JOptionPane jp=new JOptionPane();
-        int n=0;
-        string cadenaNumeros;
-        n= Integer.parseInt(jp.showInputDialog(null,"IMGRESE laa camtodad de numeros "));
-        int[] numeros=new int[n];
-        for (int i = 0; i < numeros.length; i++) {
-            numeros[i]= Integer.parseInt(jp.showInputDialog(null,"INGRESE NUMERO"));
+// public class jop{
+//     public static void main(String[] args) {
+//         JOptionPane jp=new JOptionPane();
+//         int n=0;
+//         string cadenaNumeros;
+//         n= Integer.parseInt(jp.showInputDialog(null,"IMGRESE laa camtodad de numeros "));
+//         int[] numeros=new int[n];
+//         for (int i = 0; i < numeros.length; i++) {
+//             numeros[i]= Integer.parseInt(jp.showInputDialog(null,"INGRESE NUMERO"));
         
-        }
-        for (int i = 0; i < numeros.length; i++) {
-            cadenaNumeros= cadenaNumeros
-        }
+//         }
+//         for (int i = 0; i < numeros.length; i++) {
+//             cadenaNumeros= cadenaNumeros
+//         }
 
-    }
-}
+//     }
+// }
